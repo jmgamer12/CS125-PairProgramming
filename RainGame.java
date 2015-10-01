b7fd814082ca14e9395995f6bb21eaa9abb1e68a
Nick Magerko <nmagerko@gmail.com>
2015-09-30 23:26:56 -0500

    Refactored code to increase maintainability

--- RainGame.java
+++ RainGame.java
@@ -1,57 +1,139 @@
 //UIUC CS125 FALL 2014 MP. File: RainGame.java, CS125 Project: PairProgramming, Version: 2015-09-28T22:11:14-0500.040275944
+
+
 /**
- * @author jmgreen5
+ * A game of raining numbers.
+ * @author jmgreen5 magerko2
  */
 public class RainGame {
-
-	public static void main(String[] args) {
-		// To get points type your netids above (CORRECTLY-Please double check your partner correctly spells your netid correctly!!)
-		// Your netid is the unique part of your @illinois email address
-		// Do not put your name or your UIN. 
-		// REMEMBER TO COMMIT this file...
 	
-		int x=0, y=0, dx=0, dy=0, score = 0;
-		String text = "";
-		long startTime =System.currentTimeMillis();
+	/* internal objects */
+	private static class Color {
+		public int red;
+		public int green;
+		public int blue;
+		
+		public Color(int red, int green, int blue) {
+			this.red = red;
+			this.blue = blue;
+			this.green = green;
+		}	
+	}
+	
+	private static class Position2d {
+		public int x;
+		public int y;
+		
+		public Position2d(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}	
+		
+		@Override
+		public Position2d clone() {
+			return new Position2d(x, y);
+		}
+	}
+	
+	private static class Velocity2d {
+		public int dx;
+		public int dy;
+		
+		public Velocity2d(int dx, int dy) {
+			this.dx = dx;
+			this.dy = dy;
+		}
+		
+		@Override
+		public Velocity2d clone() {
+			return new Velocity2d(dx, dy);
+		}
+	}
+	
+	/* constant declarations */
+	private static final Position2d INITIAL_POSITION = new Position2d(0, Zen.getZenHeight() / 2);
+	private static final Velocity2d INITIAL_VELOCITY = new Velocity2d(0, 1);
+	
+	private static final int BASE_SCORE = 3000;
+	
+	private static final Color TEXT_COLOR = new Color(255, 0, 255);
+	private static final Color BACKGROUND_COLOR = new Color(0, 255, 0);
+	private static final Position2d LEVEL_POSITION = new Position2d(10, 60);
+	private static final Position2d SCORE_POSITION = new Position2d(10, 120);
+	
+	/* helper method declarations */
+	private static String generateRandomText() {
+		return String.valueOf((int) (Math.random() * 999));
+	}
+	
+	private static int calculateScore(long timeElapsed) {
+		if (timeElapsed <= 0) return 0;
+		return (int) (BASE_SCORE / timeElapsed);
+	}
+	
+	private static void updateZenInterface(String text, Position2d position) {
+		Zen.setColor(TEXT_COLOR.red, TEXT_COLOR.green, TEXT_COLOR.blue);
+		Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
+		Zen.setColor(BACKGROUND_COLOR.red, BACKGROUND_COLOR.green, BACKGROUND_COLOR.blue);
+		Zen.drawText(text, position.x, position.y);
+		Zen.drawText("Level: 0", LEVEL_POSITION.x, LEVEL_POSITION.y);
+		Zen.drawText("Score: 0", SCORE_POSITION.x, SCORE_POSITION.y);
+	}
+
+	/* program entry-point */
+	public static void main(String[] args) {
 		
 		Zen.setFont("Helvetica-64");
+
+		int score = 0;
+		String text = "";
+		Position2d position = INITIAL_POSITION.clone();
+		Velocity2d velocity = INITIAL_VELOCITY.clone();
+		
+		long startTime = System.currentTimeMillis();
+		
 		while (Zen.isRunning()) {
-
-			if (text.length() == 0) {
-				x = 0;
-				y = Zen.getZenHeight() / 2;
-				dx = 0;
-				dy = 1;
-				text = "" + (int) (Math.random() * 999);
-				long elapsed = System.currentTimeMillis() - startTime;
+			
+			// reset condition
+			if (text.isEmpty()) {
+				// reset the visible text
+				text = generateRandomText();
+				
+				// reset the text position and rate
+				position = INITIAL_POSITION.clone();
+				velocity = INITIAL_VELOCITY.clone();
+				
+				// calculate the resulting score and reset the timer
+				long timeElapsed = System.currentTimeMillis() - startTime;
 				startTime = System.currentTimeMillis();
-				score += 3000 / elapsed;
+				score += calculateScore(timeElapsed);
 			}
-			Zen.setColor(255, 0, 255);
-			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
 
-			Zen.setColor(0, 255, 0);
-			Zen.drawText(text, x, y);
 			
-			Zen.drawText("Level: 0",10,60);
-			Zen.drawText("Score: 0",10,120);
+			// perform necessary UI updates
+			updateZenInterface(text, position);
 			
-			x += dx;
-			y += dy;
+			// increase position by current velocity
+			position.x += velocity.dx;
+			position.y += velocity.dy;
 			
-			// Find out what keys the user has been pressing.
+			// find out what keys the user has been pressing.
 			String user = Zen.getEditText();
-			// Reset the keyboard input to an empty string
-			// So next iteration we will only get the most recently pressed keys.
+			
+			// reset the keyboard input to an empty string
+			// so next iteration we will only get the most recently pressed keys.
 			Zen.setEditText("");
 			
-			for(int i=0;i < user.length();i++) {
+			for(int i=0;i < user.length(); i++) {
+				// check to see if the user entered the correct character
+				// if correct, remove that character
 				char c = user.charAt(i);
 				if(c == text.charAt(0))
-					text = text.substring(1,text.length()); // all except first character
+					text = text.substring(1,text.length());
 			}
 			
-			Zen.sleep(90);// sleep for 90 milliseconds
+			// sleep for 90 milliseconds
+			Zen.sleep(90);
 
 		}
 	}
